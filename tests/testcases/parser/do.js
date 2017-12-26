bc91dbb8396ca377f57dee4bb7bbbc7bd19d37fe
Peter van der Zee <github-public@qfox.nl>
2017-12-26 00:03:05 +0000

    do-while error test case

--- tests/testcases/parser/do.js
+++ tests/testcases/parser/do.js
@@ -9,10 +9,35 @@ module.exports = (describe, test) => describe('dowhile statement', _ => {
 
   test('simple while',{
     code: 'do foo\nwhile (bar);',
-    ast: {type: 'Program', body: [
-      {type: 'DoWhileStatement', body: {type: 'ExpressionStatement', expression: {type: 'Identifier', name: 'foo'}}, test: {type: 'Identifier', name: 'bar'}},
-    ]},
+    ast: {type: 'Program', body: [{
+      type: 'DoWhileStatement',
+      body: {
+        type: 'ExpressionStatement',
+        expression: {type: 'Identifier', name: 'foo'},
+      },
+      test: {type: 'Identifier', name: 'bar'},
+    }]},
     tokens: [$IDENT, $IDENT, $ASI, $IDENT, $PUNCTUATOR, $IDENT, $PUNCTUATOR, $PUNCTUATOR],
   });
-  // TODO: error for `do foo while (bar);`
+
+  test('no asi error', {
+    code: 'do foo; while (bar);',
+    ast: {type: 'Program', body: [{
+      type: 'DoWhileStatement',
+      body: {
+        type: 'ExpressionStatement',
+        expression: {type: 'Identifier', name: 'foo'},
+      },
+      test: {type: 'Identifier', name: 'bar'},
+    }]},
+    desc: 'the problem is that ASI is only applied after newlines or before } or EOF and that this satisfies neither',
+    tokens: [$IDENT, $IDENT, $PUNCTUATOR, $IDENT, $PUNCTUATOR, $IDENT, $PUNCTUATOR, $PUNCTUATOR],
+  });
+
+  test('asi error', {
+    code: 'do foo while (bar);',
+    throws: 'ASI',
+    desc: 'the problem is that ASI is only applied after newlines or before } or EOF and that this satisfies neither',
+    tokens: [$IDENT, $IDENT, $IDENT, $PUNCTUATOR, $IDENT, $PUNCTUATOR, $PUNCTUATOR],
+  });
 });
