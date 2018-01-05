90685f6025ef105360826ba0127ecea9003e76b0
Peter van der Zee <github-public@qfox.nl>
2018-01-05 21:30:35 +0000

    todo: in operator tests

---
+++ tests/testcases/parser/expressions/in.js
@@ -0,0 +1,14 @@
+let {
+  $ASI,
+  $IDENT,
+  $PUNCTUATOR,
+} = require('../../../../src/zetokenizer');
+
+
+module.exports = (describe, test) => describe('in operator', _ => {
+
+});
+
+// TODO: various cases of checking that `in` parses properly (LF_NO_IN), including nested stuff and resetting of flag
+// TODO: various for-in edge cases
+// TODO: `in` whitespace edge cases
