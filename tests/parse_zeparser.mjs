bb555946260ea7c7b8811c0730714eb19d1e9e98
Peter van der Zee <github-public@qfox.nl>
2019-09-29 14:44:27 +0200

    No need to collect tokens while benchmarking

--- tests/parse_zeparser.mjs
+++ tests/parse_zeparser.mjs
@@ -1,10 +1,10 @@
-import {COLLECT_TOKENS_SOLID, GOAL_MODULE, GOAL_SCRIPT} from "../src/zetokenizer.mjs";
+import {COLLECT_TOKENS_SOLID, COLLECT_TOKENS_NONE , GOAL_MODULE, GOAL_SCRIPT} from "../src/zetokenizer.mjs";
 
 function testZeParser(zeparser, code, testVariant, enableAnnexb) {
   return zeparser(
     code,
     testVariant === 'module' ? GOAL_MODULE : GOAL_SCRIPT,
-    COLLECT_TOKENS_SOLID,
+    COLLECT_TOKENS_NONE,
     {
       strictMode: testVariant === 'strict',
       webCompat: enableAnnexb || testVariant === 'web',
