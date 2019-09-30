efb44a44af87885193abedf34af4ee2f285d9f31
Peter van der Zee <github-public@qfox.nl>
2019-09-30 10:19:37 +0200

    Add deltas as they are a more relevant metric than absolute ms right now

--- tests/perf.mjs
+++ tests/perf.mjs
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import {performance} from 'perf_hooks';
 
 import {testZeParser} from './parse_zeparser.mjs';
 import {testBabel} from './parse_babel.mjs';
@@ -43,14 +44,24 @@ const ZEPARSER_PROD_FILE = '../build/build_w_ast.mjs';
 
   files.forEach(({path, code, mode}) => {
     console.group('File:', code.length, 'bytes:', path);
+    let ztime = 0;
     parsers.forEach(({name, parse}) => {
-      console.time('Parse time for ' + name);
+      let t1 = performance.now();
       try {
         parse(code, mode);
       } catch (e) {
         console.log('Failed!', name, '->', path, '::', e.message);
       }
-      console.timeEnd('Parse time for ' + name);
+      let t2 = performance.now();
+      let time = t2 - t1;
+      if (ztime === 0) {
+        if (name !== 'zepar') throw 'z should run first so we can delta against the rest';
+        ztime = time;
+        console.log('Parse time for ' + name + ': ' + (Math.round(ztime * 100)/100) + ' ms');
+      } else {
+        let delta = time - ztime;
+        console.log('Parse time for ' + name + ': ' + (Math.round(time * 100)/100) + ' ms (' + (delta > 0 ? '+' : '') + (Math.round(delta * 100)/100) + ' ms)');
+      }
     });
     console.groupEnd();
   });
